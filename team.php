f0e0c1d4a12a63eab766cc23de1a70364f097ce9
nataliaodhiambo <natalyafaith.odhiambo@strathmore.edu>
2024-07-07 16:57:01 +0300

    added the shortened php line of code

--- team.php
+++ team.php
@@ -5,33 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Our salon team</title>
-    <style>
-       #uniqueId
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f9f5ec; /* warm beige for background */
-            color: #333; /* dark text color */
-        }
-        .top-bar{
-            text-align: center;
-            padding: 10px;
-        }
-        .top-bar a{
-            color: #333;
-            text-decoration: none;
-            padding: 10px;
-            border: 1 px solid #ddd;
-        }
-        .top-bar a:hover{
-            background-color: #a02020;
-        }
-        main section{
-            margin-bottom: 20px;
-        }
-        h2{
-            margin-bottom: 10px;
-        }
-    </style>
+
     </head>
     <body>
 <?php include_once("templates/nav.php");?>
