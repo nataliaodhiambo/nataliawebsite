988ce4ee197983b4eacb563a393834a69a9b817d
nataliaodhiambo <natalyafaith.odhiambo@strathmore.edu>
2024-07-07 17:18:39 +0300

    deleted the link for the stylists page

--- templates/nav.php
+++ templates/nav.php
@@ -1,7 +1,6 @@
 <div class="top-bar">
         <a href="homepage.php">Home</a>
         <a href="about.php">About Us</a>
-        <a href="team.php">Our Stylists</a>
         <a href="gallery.php">Gallery</a>
         <a href="appointments.php">Book an Appointment</a>
         <a href="feedback.php">Feedback Form</a>
