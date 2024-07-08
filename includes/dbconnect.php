7a156075dcacc2d49700cb97e8fda629f65f04c5
nataliaodhiambo <natalyafaith.odhiambo@strathmore.edu>
2024-07-08 10:31:02 +0300

    for connecting the database to the website

---
+++ includes/dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$dbname= "customerreviews";
+$password="";
+
+// Create connection
+$conn = new mysqli($servername, $username, $dbname, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+/* echo "Connected successfully";
+?>
